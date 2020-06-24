b62c65069e1c09bb265cd70a0f7fe8f02e8d3554
Alex <alexnem@inbox.ru>
2020-06-24 14:57:27 +0300

    video lesson

---
+++ src/components/HeaderBlock/HeaderBlock.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HeaderBlock.css';
+
+const HeaderBlock = () => {
+  return (
+    <div>
+      <div>
+        <h1>Учите слова онлайн</h1>
+        <p>Воспользуйетсь карточками</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderBlock;
