f9f459e265883916570c78d1cc5162f717cc4e8e
Alex <alexnem@inbox.ru>
2020-06-25 02:30:46 +0300

    createContext/useContext (alert)

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import Home from './components/Pages/Home';
 import About from './components/Pages/About';
 import Profile from './components/Pages/Profile';
 import Nav from './components/Nav';
+import Alert from './components/Alert';
+import AlertState from './context/alert/alertState';
 
 const App = () => {
-  return (<BrowserRouter>
-      <Nav />
-      <div className='container pt-4'>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/profile/:name' component={Profile} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+  return (
+    <AlertState>
+      <BrowserRouter>
+        <Nav />
+        <div className='container pt-4'>
+          <Alert alert={ { text: 'test' } } />
+          <Switch>
+            <Route path='/' exact component={ Home } />
+            <Route path='/about' component={ About } />
+            <Route path='/profile/:name' component={ Profile } />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 };
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = () => {
       <img src={''} alt='' className='card-img-top' />
       <div className='card-body'>
         <h5 className='card-title'>React</h5>
-        <Link to={'/profile/' + 'react'} className='btn btn-primary'>Открыть</Link>
+        <Link to={'/profile/'} className='btn btn-primary'>Открыть</Link>
       </div>
     </div>
   );
